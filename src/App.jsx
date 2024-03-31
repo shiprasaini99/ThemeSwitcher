e37f9f456027e7b32c4b7d4ca9e279f1338376a9
sainishipra99 <shiprasaini2012@gmail.com>
2024-03-31 15:29:10 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,42 @@
-import { useState } from 'react'
+
+import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './context/Theme'
-import { useEffect } from 'react';
-import ThemeBtn from './components/ThemeBtn';
-import Card from './components/Card';
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
+
 function App() {
-  const [themeMode, setThemeMode] =useState("light");
+  const [themeMode, setThemeMode] = useState("light")
 
-
-  const lightTheme =()=>{
+  const lightTheme = () => {
     setThemeMode("light")
   }
-  const darkTheme =()=>{
+
+  const darkTheme = () => {
     setThemeMode("dark")
   }
 
-  //actual change in a theme
+  // actual change in theme
+
   useEffect(() => {
-    document.querySelector('html').classList.remove("light")
+    document.querySelector('html').classList.remove("light", "dark")
     document.querySelector('html').classList.add(themeMode)
   }, [themeMode])
   
+
   return (
     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
-     
-<div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        <ThemeBtn/>
-                    </div>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
 
-                    <div className="w-full max-w-sm mx-auto">
-                       <Card/>
-                    </div>
-                </div>
-            </div>
-
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
     </ThemeProvider>
   )
 }
--- src/context/Theme.js
+++ src/context/Theme.js
@@ -1,15 +1,12 @@
-import { useContext } from "react";
-import { createContext } from "react";
-
- 
+import { createContext, useContext } from "react";
 
 export const ThemeContext = createContext({
-    themeMode : 'light',
-    darkTheme:()=>{},
-    lightTheme: ()=>{}
-});
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+})
 
-export const  ThemeProvider = ThemeContext.Provider
+export const ThemeProvider = ThemeContext.Provider
 
 export default function useTheme(){
     return useContext(ThemeContext)
